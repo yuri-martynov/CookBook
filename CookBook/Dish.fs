0313027dca0afbc419e8ed0898a1020d9a56c4e8
Yuri Martynov <yuri.martynov@gmail.com>
2017-04-16 20:37:35 +0300

    fix

--- CookBook/Dish.fs
+++ CookBook/Dish.fs
@@ -3,12 +3,16 @@
 open Types
 open Functions
 
-let findByIngredients (getDishesByIngredients : getDishesByIngredients) (getDishById: getDishById) (products: seq<string>): Async<string> = async {
-    let! dishIds = getDishesByIngredients products
-    let id = dishIds |> Seq.tryHead
-    match id with
-    |Some id -> return! Ingredients.get getDishById id
-    |None -> return "нет("
-}
+let findByIngredients 
+    (getDishesByIngredients : getDishesByIngredients) 
+    (getDishById: getDishById) (products: seq<string>)
+    : Async<string> = 
+    async {
+        let! dishIds = getDishesByIngredients products
+        let id = dishIds |> Seq.tryHead
+        match id with
+        |Some id -> return! Ingredients.get getDishById id
+        |None -> return "нет("
+    }
     
 
