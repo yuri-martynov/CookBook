dd264b23255e39154a441e1b1daad66778694c91
Yuri Martynov <yuri.martynov@gmail.com>
2017-04-16 15:30:03 +0300

    fix

--- DataAccess/Code/FilesDataAccess.cs
+++
@@ -1,9 +0,0 @@
-﻿using DataAccess.Properties;
-
-namespace DataAccess
-{
-    public sealed class FilesDataAccess : IFilesDataAccess
-    {
-        public string Get(string id) => Resources.ResourceManager.GetString(id);
-    }
-}
