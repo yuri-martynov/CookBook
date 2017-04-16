034adb9e30b0ba0a64d96753ece1fbc7810d4389
Yuri Martynov <yuri.martynov@gmail.com>
2017-04-16 15:02:55 +0300

    fix

--- CookBook/Data.fs
+++ CookBook/Data.fs
@@ -6,5 +6,6 @@ open FSharp.Data
 type DishXml = XmlProvider<"../recipe/Db/борщ.xml">
 
 let get id : DishXml.Dish =
-    let str = System.IO.File.ReadAllText """D:\home\site\wwwroot\recipe\db\""" + id + ".xml"
+    let fileName = """D:\home\site\wwwroot\recipe\db\""" + id + ".xml"
+    let str = System.IO.File.ReadAllText fileName
     DishXml.Parse str
