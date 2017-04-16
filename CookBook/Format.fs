3f51e0ad72861baeb7f46e2878c84fa6421b530d
Yuri Martynov <yuri.martynov@gmail.com>
2017-04-16 18:30:02 +0300

    cnt

--- CookBook/Format.fs
+++ CookBook/Format.fs
@@ -5,11 +5,15 @@ open Types
 
 let quantity (x : Quantity) =
     match x with
+    |Spoons 1.0 -> "одна ложка"
+    |Spoons 2.0 -> "две ложки"
+    |Spoons 3.0 -> "три ложки"
+    |Spoons 4.0 -> "четыре ложки"
     |Spoons s -> s.ToString() + " ложек"
     |x -> x.ToString()
 
 let product (x: Product) =
-    sprintf "%s %s" x.name (quantity x.quantity)
+    sprintf "%s - %s" x.name (quantity x.quantity)
 
 
 let list (separator: string) (lst: seq<_>) : string =
