0ff50b45b5e5d8d595852b37555bba42080dc96f
Yuri Martynov <yuri.martynov@gmail.com>
2017-04-17 19:04:40 +0300

    avg

--- CookBook/CompareTime.fs
+++ CookBook/CompareTime.fs
@@ -16,14 +16,21 @@ let get (getDishById: getDishById) (dishIds: seq<string>) : Async<string> = asyn
         |> Seq.map getIdAndTime
         |> Async.Parallel
 
-    let format ((d,t) : (Dish*TimeSpan)) =
-        d.name + " - " + (Format.duration t)
+    let times = dishesWithTime |> Seq.map (snd >> (fun t -> t.TotalMinutes))
+    let avg = times |> Seq.average
+    let div = times |> Seq.map (fun t -> Math.Abs(t-avg)) |> Seq.max
+    
+    if div < avg * 0.2 then
+        return "примерно одинакого в среднем: " + ((TimeSpan.FromMinutes avg) |> Format.duration)
+    else
+        let format ((d,t) : (Dish*TimeSpan)) =
+            d.name + " - " + (Format.duration t)
 
-    return 
-        dishesWithTime
-        |> Seq.sortBy snd
-        |> Seq.map format
-        |> Format.list ". "
+        return 
+            dishesWithTime
+            |> Seq.sortBy snd
+            |> Seq.map format
+            |> Format.list ". "
 
 }
 
