7e48824ca2dbea0c113c1a10967377fe113b3f29
Yury Martynov <yuri.martynov@gmail.com>
2017-04-22 18:35:09 +0300

    fix

--- Data/Products/Liquids.fs
+++ Data/Products/Liquids.fs
@@ -6,5 +6,5 @@ open Types
 module GlobalLiquids =
     let вода = Whole "вода"
     let ``растительное масло``= Whole "растительное масло"
-    let ``масло оливковое``= Whole "оливковое масло"
+    let ``масло оливковое``= Whole "масло оливковое"
     let пиво = Whole "пиво"
