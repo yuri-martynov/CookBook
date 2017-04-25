7e43bc0bde49ee9f507c04f1f93fdafe5b490213
Yury Martynov <yuri.martynov@gmail.com>
2017-04-25 22:36:14 +0300

    reflection

--- Data/DishDataAccess.fs
+++ Data/DishDataAccess.fs
@@ -1,17 +1,34 @@
 ﻿module DishDataAccess
 
+open System.Reflection
+open Microsoft.FSharp.Reflection
 open Types
-
 open Dishes
+
 
 let private all p s =
     not <| Seq.exists (not << p) s 
 
-let private dishes : Dish list = 
-    [ борщ; щи; рассольник
-    ; рататуй
-    ; маффин
-    ]
+
+let letBindings<'t> (assembly: Assembly) : 't array =
+    let publicTypes = assembly.GetExportedTypes ()
+    let modules = publicTypes |> Array.filter FSharpType.IsModule
+    let members = modules |> Array.collect (fun m -> m.GetMembers ())
+
+    let valueOfBinding (mi : MemberInfo) =
+        let property = mi.Name
+        mi.DeclaringType.GetProperty(property).GetValue null
+
+    members 
+        |> Array.map valueOfBinding 
+        |> Array.choose (fun o ->  match o with
+                                    | :? 't as x -> Some x
+                                    | _ -> None)
+
+
+let private dishes : Dish array = 
+    let assembly = typeof<Data.AssemblyInfo.MarkerType>.Assembly
+    assembly |> letBindings<Dish> 
 
 let private dict : Map<string, Dish> = 
     dishes  
