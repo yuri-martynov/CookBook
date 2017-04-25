031f811ee4742faab0d394717e2af839f1f5072a
Yury Martynov <yuri.martynov@gmail.com>
2017-04-25 23:00:58 +0300

    fix 5

--- Data/DishDataAccess.fs
+++ Data/DishDataAccess.fs
@@ -26,12 +26,12 @@ let letBindings<'t> (assembly: Assembly) : 't array =
         |> Array.choose valueOfBinding
 
 
-let private dishes : Dish array = 
+let private dishes () : Dish array = 
     let assembly = typeof<Data.AssemblyInfo.MarkerType>.Assembly
     assembly |> letBindings<Dish> 
 
 let private dict : Map<string, Dish> = 
-    dishes  
+    dishes()  
     |> Seq.map (fun d -> (d.name, d))
     |> Map.ofSeq
 
@@ -39,6 +39,6 @@ let getById id =
     dict |> Map.find id
 
 let getByIngredients (productNames: seq<string>) : seq<string> =
-    dishes
+    dishes()
     |> Seq.filter (fun d -> productNames |> all (fun p -> d |> Utils.ingredients |> Seq.collect Utils.productsQuantities |> Seq.map (fun i -> Utils.productName i.product ) |> Seq.contains p))
     |> Seq.map (fun d -> d.name)
